ed325ddc39def10334f2e86805bae7ced0d536ce
Felipe Barzilai <barzilaifelipe@gmail.com>
2025-09-24 18:37:17 -0300

    Delete estruturas_de_condicao directory

--- estruturas_de_condicao/#Problema2(if).py
+++
@@ -1,8 +0,0 @@
-#Problema2(if)
-
-horas = int(input("Quantas foram as horas trabalhadas no mês? "))
-salarioBruto = horas*19.50
-
-if salarioBruto > 2500:
- salarioLiquido = salarioBruto - (salarioBruto*0.25)
- print(f"Seu salário líquido é {salarioLiquido:.2f}")
